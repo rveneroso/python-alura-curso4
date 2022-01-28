28a7268042187c801fe195fab4590e3da560226f
Renato Veneroso <rveneroso@gmail.com>
2022-01-28 09:42:45 -0300

    Criação da superclasse Programa, transformação dos atributos nome e likes em protected e inclusão de herança nas classes Filme e Serie

--- modelo.py
+++ modelo.py
@@ -1,46 +1,40 @@
-class Filme:
+class Programa:
+    def __init__(self, nome, ano):
+        #
+        # Em Python, um único underline (_) à frente do nome do atributo indica que o atributo é protected.
+        #
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    def dar_like(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self._likes
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        self.__nome = nome.title
+        self._nome = nome.title()
         self.ano = ano
         self.temporadas = temporadas
-        self.__likes = 0
-
-    def dar_like(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome.title()
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
+        self._likes = 0
 
 profecia = Filme('a profecia', 1977, 120)
 profecia.dar_like()
