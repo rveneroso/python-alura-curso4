9f5081342faac5458bfb4aff708acdb2596558b3
Renato Veneroso <rveneroso@gmail.com>
2022-01-28 09:20:03 -0300

    Tornando os atributos nome e likes privados nas classes Filme e Serie

--- modelo.py
+++ modelo.py
@@ -1,17 +1,52 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome
+        self.__nome = nome.title
         self.ano = ano
         self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome.title()
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+    @property
+    def likes(self):
+        return self.__likes
 
 profecia = Filme('a profecia', 1977, 120)
-print(f'O filme {profecia.nome} foi lançado em {profecia.ano} e tem a duração de {profecia.duracao} minutos')
+profecia.dar_like()
+profecia.dar_like()
+print(f'O filme {profecia.nome} foi lançado em {profecia.ano}, tem a duração de {profecia.duracao} minutos e já recebeu {profecia.likes} likes')
 
 bbt = Serie('the big bang theory',2007, 2019)
-print(f'A série {bbt.nome} foi lançada em {bbt.ano} e durou {bbt.temporadas} temporadas')
+bbt.dar_like()
+print(f'A série {bbt.nome} foi lançada em {bbt.ano}, tem a duração de {bbt.temporadas} temporadas e já recebeu {bbt.likes} likes')
